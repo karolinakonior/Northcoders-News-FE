9f5b19d9008e2e31806799c288869e619ac98a47
karolinakonior <karolina.koniorr@gmail.com>
2023-10-26 14:31:13 +0100

    Added functionallity to delete comment

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,24 +1,32 @@
 import { useEffect, useState, useContext } from "react";
-import { getComments } from '../api/api';
+import { deleteComment, getComments } from '../api/api';
 import Card from 'react-bootstrap/Card';
-import ReactTimeAgo from 'react-time-ago'
-import vote from '../images/vote-icon.png'
+import ReactTimeAgo from 'react-time-ago';
+import vote from '../images/vote-icon.png';
 import Spinner from 'react-bootstrap/Spinner';
 import { UsernameContext } from "../context/username-context";
 import PostComment from './PostComment';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import remove from '../images/remove.png';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
 
 const CommentsList = ({ article_id }) => {
     const [comments, setComments] = useState([])
     const [isLoading, setIsLoading] = useState(true)
-    const {username, setUsername} = useContext(UsernameContext)
+    const { username, setUsername } = useContext(UsernameContext)
+    const [message, setMessage] = useState("")
+    const [error, setError] = useState("")
+    const [isDisabled, setIsDisabled] = useState(null)
 
-    function parseCommentCreatedAt(comment){
+    function parseCommentCreatedAt(comment) {
         return Date.parse(comment.created_at);
     }
 
     useEffect(() => {
+        setIsDisabled(true)
         setIsLoading(true)
         getComments(article_id)
             .then((response) => {
@@ -26,9 +34,26 @@ const CommentsList = ({ article_id }) => {
                 setComments(response.data.comments)
             })
             .catch((error) => {
-                console.log(error)
+                setError("Something went wrong, please try again.")
             })
     }, [])
+
+    const handleClick = (comment_id) => {
+        setIsDisabled(true)
+        setMessage("Comment is being deleted - please wait.")
+        deleteComment(comment_id)
+            .then(() => {
+                getComments(article_id)
+                    .then((response) => {
+                        setIsLoading(false)
+                        setComments(response.data.comments)
+                        setMessage("Comment succesfully deleted!")
+                    })
+                    .catch((error) => {
+                        setError("Something went wrong, please try again.")
+                    })
+            })
+    }
 
     if (isLoading) {
         return <>
@@ -42,7 +67,9 @@ const CommentsList = ({ article_id }) => {
 
     return (
         <>
-            {username ? <PostComment article_id={article_id} comments={comments} setComments={setComments} /> : <Button variant="dark"><Link style={{color: "white", textDecoration: "none"}} to="/signin">Sign in to post a comment!</Link></Button>}
+            {username ? <PostComment article_id={article_id} comments={comments} setComments={setComments} /> : <Button variant="dark"><Link style={{ color: "white", textDecoration: "none" }} to="/signin">Sign in to post a comment!</Link></Button>}
+            <Card.Title class="card-title h5" style={{ paddingLeft: '1rem', paddingBottom: '1rem' }}>{message ? message : null}</Card.Title>
+            <Card.Title class="card-title h5" style={{ paddingLeft: '1rem', paddingBottom: '1rem' }}>{error ? error : null}</Card.Title>
             {comments.map((comment) => {
                 return (
                     <Card style={{ width: '100%' }} key={comment.comment_id}>
@@ -60,8 +87,21 @@ const CommentsList = ({ article_id }) => {
                             <Card.Text>
                                 {comment.body}
                             </Card.Text>
-                            <Card.Subtitle className="mb-2 text-muted">
-                                <img id="comment-icon" src={vote} alt="Image representing votes count" />{comment.votes}</Card.Subtitle>
+                            <section className="container">
+                                <section className="wrapper">
+                                    <section className="box a">
+                                        <Card.Subtitle className="mb-2 text-muted">
+                                            <img id="comment-icon" src={vote} alt="Image representing votes count" />{comment.votes}</Card.Subtitle>
+                                    </section>
+                                    <section className="box b">
+                                        <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{username ? (username === comment.author ? "Delete" : "You can delete only your comment!") : "Sign in to delete"}</Tooltip>}>
+                                            <button className="button" onClick={(() => { handleClick(comment.comment_id) })} 
+                                            disabled={username === comment.author && isDisabled ? false : true}>
+                                                <img id="remove-icon" src={remove} style={{ color: "white", height: "1.5rem" }} alt="Image representing delete icon" /></button>
+                                        </OverlayTrigger>
+                                    </section>
+                                </section>
+                            </section>
                         </Card.Body>
                     </Card>
                 )
